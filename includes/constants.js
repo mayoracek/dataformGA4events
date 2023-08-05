405a05b41ed84ee698155a2b5001dc60fa17b344
Mayo Racek <datahelp@powerfuldata.io>
2023-08-05 08:50:53 +0000

    First dataform workflow - page_views.sqlx

---
+++ includes/constants.js
@@ -0,0 +1,4 @@
+const GA4_DATABASE = 'ga4-export-bigquery-308108';
+const GA4_DATASET = 'analytics_265608129';
+const GA4_TABLE = 'events_20230120';
+module.exports = {GA4_DATABASE, GA4_DATASET, GA4_TABLE}
